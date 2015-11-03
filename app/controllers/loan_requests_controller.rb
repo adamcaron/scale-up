ba78de15b17d7adcad283f0b8b44e05d43165131
Adam Caron <adamwcaron@gmail.com>
2015-11-03 13:42:57 -0700

    Change number of items per page

--- app/controllers/loan_requests_controller.rb
+++ app/controllers/loan_requests_controller.rb
@@ -2,7 +2,7 @@ class LoanRequestsController < ApplicationController
   before_action :set_loan_request, only: [:update, :show]
 
   def index
-    @loan_requests = LoanRequest.paginate(:page => params[:page], :per_page => 5)
+    @loan_requests = LoanRequest.paginate(:page => params[:page], :per_page => 9)
   end
 
   def create
